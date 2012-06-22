362d2de52febda26ff6790d4fc799f22a669ddf1
Jurgens du Toit <jrgns@jrgns.net>
2012-06-22 08:39:30 +0200

    Implement the BindingInterface

--- Bindings/Binding.php
+++ Bindings/Binding.php
@@ -13,9 +13,10 @@
  * @link       http://backend-php.net
  */
 namespace Backend\Modules\Bindings;
+use \Backend\Interfaces\BindingInterface;
 use \Backend\Modules\Exception as ModuleException;
 /**
- * Abstract class for Bindings
+ * Abstract class for Data Bindings
  *
  * Bindings act as a transport layer which can be used to perform CRUD actions
  * on a resource. It's typically used by Models to maintain their state on an
@@ -32,7 +33,7 @@ use \Backend\Modules\Exception as ModuleException;
  * @license    http://www.opensource.org/licenses/mit-license.php MIT License
  * @link       http://backend-php.net
  */
-abstract class Binding
+abstract class Binding implements BindingInterface
 {
     /**
      * The name of the class this binding operates on.
@@ -57,77 +58,4 @@ abstract class Binding
         }
         $this->className = $settings['class'];
     }
-
-    /**
-     * Find multiple instances of the resource.
-     *
-     * Don't specify any criteria to retrieve a full list of instances.
-     *
-     * @param array $conditions An array of conditions on which to filter the list.
-     * @param array $options    An array of options.
-     *
-     * @return array An array of representations of the resource.
-     */
-    abstract public function find(
-        array $conditions = array(), array $options = array()
-    );
-
-    /**
-     * Create an instance on the source, and return the instance.
-     *
-     * @param array $data The data to create a new resource.
-     *
-     * @return \Backend\Interfaces\ModelInterface The created model.
-     * @throws \Backend\Modules\Exception When the resource can't be created.
-     */
-    abstract public function create(array $data);
-
-    /**
-     * Read and return the single, specified instance of the resource.
-     *
-     * @param mixed $identifier The unique identifier for the instance, or an
-     * array containing criteria on which to search for the resource.
-     *
-     * @return \Backend\Interfaces\ModelInterface The identified model.
-     * @throws \Backend\Modules\Exception When the resource can't be found.
-     */
-    abstract public function read($identifier);
-
-    /**
-     * Refresh the specified instance on the source.
-     *
-     * This function is the logical counterpart to update, and receives data from
-     * the source.
-     *
-     * @param \Backend\Interfaces\ModelInterface &$model The model to refresh.
-     * Passed by reference.
-     *
-     * @return boolean If the refresh was successful or not.
-     * @throws \Backend\Modules\Exception When the resource can't be refreshed.
-     */
-    abstract public function refresh(\Backend\Interfaces\ModelInterface &$model);
-
-    /**
-     * Update the specified instance of the resource.
-     *
-     * This function is the logical counterpart to refresh, and sends data to
-     * the source.
-     *
-     * @param \Backend\Interfaces\ModelInterface &$model The model to update.
-     * Passed by reference.
-     *
-     * @return boolean If the update was successful or not.
-     * @throws \Backend\Modules\Exception When the resource can't be updated.
-     */
-    abstract public function update(\Backend\Interfaces\ModelInterface &$model);
-
-    /**
-     * Delete the specified instance of the resource
-     *
-     * @param \Backend\Interfaces\ModelInterface &$model The model to delete
-     *
-     * @return boolean If the deletion was succesful or not.
-     * @throws \Backend\Modules\Exception When the resource can't be deleted.
-     */
-    abstract public function delete(\Backend\Interfaces\ModelInterface &$model);
 }
