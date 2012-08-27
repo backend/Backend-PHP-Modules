c753549b80a6466e244fa969cca686c70c6ae043
Jurgens du Toit <jrgns@jrgns.net>
2012-08-27 05:17:44 +0200

    Set Doctrine Options

--- Bindings/DoctrineBinding.php
+++ Bindings/DoctrineBinding.php
@@ -69,12 +69,30 @@ class DoctrineBinding extends DatabaseBinding
     protected function init(array $connection)
     {
         if (empty(self::$em)) {
-            //Setup Doctrine
+            // Setup Doctrine
             $isDevMode = (BACKEND_SITE_STATE != 'production');
             $config    = \Doctrine\ORM\Tools\Setup::createYAMLMetadataConfiguration(
                 array(PROJECT_FOLDER . 'configs/doctrine'),
                 $isDevMode
             );
+            // Set Various Options
+            if (array_key_exists('meta_cache', $connection)) {
+                $config->setMetadataCacheImpl($connection['meta_cache']);
+                unset($connection['meta_cache']);
+            }
+            if (array_key_exists('query_cache', $connection)) {
+                $config->setQueryCacheImpl($connection['query_cache']);
+                unset($connection['query_cache']);
+            }
+            if (array_key_exists('proxy_dir', $connection)) {
+                $config->setProxyDir($connection['proxy_dir']);
+                unset($connection['proxy_dir']);
+            }
+            if (array_key_exists('proxy_namespace', $connection)) {
+                $config->setProxyNamespace($connection['proxy_namespace']);
+                unset($connection['proxy_namespace']);
+            }
+
             // obtaining the entity manager
             self::$em = EntityManager::create($connection, $config);
         }
